2361fd80d7ca59fc832329c47f9c2a7cf2b731d2
ro2co <272157218@qq.com>
2022-10-13 15:34:10 +0800

    lc977 squares-of-a-sorted-array

--- src/lc977/ans.ts
+++ src/lc977/ans.ts
@@ -1,3 +1,21 @@
 function sortedSquares(nums: number[]): number[] {
-
+  const result: number[] = []
+  let i:number = 0;
+  let j:number = nums.length -1;
+  
+  while (i<=j) {
+    if (nums[i]*nums[i] > nums[j]*nums[j]) {
+      result.unshift(nums[i]*nums[i])
+      i++
+    } else {
+      result.unshift(nums[j]*nums[j])
+      j--
+    }
+  }
+  return result
 };
+
+const arr = [-4, -1, 0, 3, 10]
+console.log(sortedSquares(arr))
+
+export {};
